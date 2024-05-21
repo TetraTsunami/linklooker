59d34819bee3f87bdf2d5aa873768ce28733ffbc
Tsuni <akamitsunami@gmail.com>
2024-05-21 17:11:23 -0500

    Update settings, token limits

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,12 +1,26 @@
-import cssText from "data-text:~content.css"
-import { useEffect, useState } from "react";
+import { Readability } from "@mozilla/readability"
+import cssText from "data-text:~content/styles.css"
+import OpenAI from "openai"
+import type { ChatCompletionMessageParam } from "openai/resources"
+import { useEffect, useState } from "react"
+
 import { sendToBackground } from "@plasmohq/messaging"
-import { Readability } from "@mozilla/readability";
-import OpenAI from 'openai';
-import type { ChatCompletionMessageParam } from "openai/resources";
-import { Storage } from "@plasmohq/storage";
-import { defaults } from "~options";
- 
+import { Storage } from "@plasmohq/storage"
+import type { PlasmoCSConfig } from "plasmo"
+
+export const config: PlasmoCSConfig = {
+    matches: ["*://*/*"],
+    exclude_matches: ["*://*.google.com/*", "*://wikipedia.com", "*://x.com"],
+}
+const defaults = {
+  baseURL: "https://api.openai.com/v1/",
+  model: "gpt-3.5-turbo",
+  prompt:
+    "Generate a descriptive and concise summary for the following web page, avoiding emojis and lists.",
+  inputTokens: 300,
+  outputTokens: 100
+}
+
 export const getStyle = () => {
   const style = document.createElement("style")
   style.textContent = cssText
@@ -15,137 +29,177 @@ export const getStyle = () => {
 
 const settings = new Storage()
 
-let hoverTarget: Element | undefined = null;
+let hoverTarget: Element | undefined = null
 
 const getConfig = async () => {
-    // Grab our config
-    const config = {
-        apiKey: await settings.get("openai-key"),
-        baseURL: await settings.get("openai-baseURL") || defaults.baseURL,
-        model: await settings.get("openai-model") || defaults.model,
-        prompt: await settings.get("system-prompt") || defaults.prompt
-    }
-    return config;
+  // Grab our config
+  const config = {
+    apiKey: await settings.get("openai-key"),
+    baseURL: (await settings.get("openai-baseURL")) || defaults.baseURL,
+    model: (await settings.get("openai-model")) || defaults.model,
+    prompt: (await settings.get("system-prompt")) || defaults.prompt,
+    inputTokens: (parseInt(await settings.get("input-tokens"))) || defaults.inputTokens,
+    outputTokens: (parseInt(await settings.get("output-tokens"))) || defaults.outputTokens,
+  }
+  return config
 }
 const summaryPopup = () => {
-    const [position, setPosition] = useState({top: 0, left: 0} as {top?: number, left?: number, right?: number, bottom?: number});
-    const [isActive, setActive] = useState(false); // Is the user trying to open the popup
-    const [isPopupReady, setPopupReady] = useState(false); // Is the popup ready to be displayed
-    const [title, setTitle] = useState("");
-    const [images, setImages] = useState([]);
-    const [description, setDescription] = useState("");
-    
-    const movePopup = (target: Element) => {
-        // Decide where to place
-        const bounds = target.getBoundingClientRect();
-        const vertical = bounds.top < window.innerHeight / 2 ? {top: bounds.bottom} : {bottom: window.innerHeight - bounds.top}
-        const horizontal = bounds.right < window.innerWidth / 2 ? {left: bounds.left} : {right: window.innerWidth - bounds.right}
-        setPosition({...vertical, ...horizontal});
+  const [position, setPosition] = useState({ top: 0, left: 0 } as {
+    top?: number
+    left?: number
+    right?: number
+    bottom?: number
+  })
+  const [isActive, setActive] = useState(false) // Is the user trying to open the popup
+  const [isPopupReady, setPopupReady] = useState(false) // Is the popup ready to be displayed
+  const [title, setTitle] = useState("")
+  const [images, setImages] = useState([])
+  const [description, setDescription] = useState("")
+
+  const movePopup = (target: Element) => {
+    // Decide where to place
+    const bounds = target.getBoundingClientRect()
+    const vertical =
+      bounds.top < window.innerHeight / 2
+        ? { top: bounds.bottom }
+        : { bottom: window.innerHeight - bounds.top }
+    const horizontal =
+      bounds.right < window.innerWidth / 2
+        ? { left: bounds.left }
+        : { right: window.innerWidth - bounds.right }
+    setPosition({ ...vertical, ...horizontal })
+  }
+
+  const updatePopup = async () => {
+    // Validate URL
+    let url = hoverTarget.getAttribute("href")
+    if (!url || url.startsWith("#") || url.startsWith("javascript")) {
+      return
     }
-
-    const updatePopup = async () => {
-        // Validate URL
-        let url = hoverTarget.getAttribute('href');
-        if (!url || url.startsWith('#') || url.startsWith('javascript')) {
-            return;
-        }
-        if (url.startsWith('/')) {
-            url = window.location.origin + url;
-        }
-        // Reset state & fetch configuration
-        setPopupReady(false);
-        setDescription("")
-        const config = await getConfig();
-        // Fetch basic data & summary
-        const resp = await sendToBackground({name: "scrape", body: {url}})
-        if (resp.error) {
-            setTitle("Error fetching data");
-            setDescription(`${resp.error}`);
-            return;
-        }
-        // Render popup with basic data
-        setTitle(resp.meta.title);
-        setDescription(resp.meta.description + "\n");
-        setImages([resp.meta.image]);
-        setActive(true);
-        // Use OpenAI API to generate a summary
-        const reader = new Readability(new DOMParser().parseFromString(resp.html, "text/html"))
-        const parsed = reader.parse();
-        const messages = [{role: "system", content: config.prompt}, { role: "user", content: parsed.textContent.slice(0, 50) }, { role: "assistant", content: resp.meta.description}] as ChatCompletionMessageParam[];
-        await streamOpenAICompletion(messages, setDescription, config).catch((e) => setDescription((prev) => prev + "\n" + "Error fetching data: " + e));
-        // Further updates to the popup are handled by the streaming function
+    if (url.startsWith("/")) {
+      url = window.location.origin + url
     }
-
-    const streamOpenAICompletion = async (messages: ChatCompletionMessageParam[], output: React.Dispatch<React.SetStateAction<string>>, config: {apiKey: string, baseURL: string, model: string}) => {
-        const openai = new OpenAI({
-            apiKey: config.apiKey,
-            baseURL: config.baseURL,
-            dangerouslyAllowBrowser: true, // it is a browser extension so this is okay!!
-        });
-        
-        const stream = await openai.chat.completions.create({
-            model: config.model,
-            messages: messages,
-            stream: true,
-            max_tokens: 50 
-        });
-        for await (const chunk of stream) {
-            if (!chunk.choices[0].delta) continue;
-            output((prev) => prev + (chunk.choices[0].delta.content || ""));
-        }
+    // Reset state & fetch configuration
+    setPopupReady(false)
+    setDescription("")
+    const config = await getConfig()
+    // Fetch basic data & summary
+    const resp = await sendToBackground({ name: "scrape", body: { url } })
+    if (resp.error) {
+      setTitle("Error fetching data")
+      setDescription(`${resp.error}`)
+      return
     }
-
-    useEffect(() => {
-        const callback = (event) => {
-            hoverTarget = (event.target as Element).closest('a');
-        }
-        document.addEventListener('mouseover', callback);
-        return () => {
-            document.removeEventListener('mouseover', callback);
-        }
-    })
-
-    useEffect(() => {
-        const callback = async (event) => {
-            if (event.key === 'Control' && hoverTarget) {
-                if (!hoverTarget) {
-                    return;
-                }
-                setActive(false);
-                movePopup(hoverTarget);
-                await updatePopup();
-            }
-        }
-        window.addEventListener('keydown', callback);
-        return () => {
-            window.removeEventListener('keydown', callback);
-        }
-    })
-
-    useEffect(() => {
-        const callback = (event) => {
-            if (event.target !== hoverTarget) {
-                setActive(false);
-            }
-        }
-        window.addEventListener('scroll', callback);
-        return () => {
-            window.removeEventListener('scroll', callback);
-        }
-    })
-
-    return (
-        <div className={`fixed min-h-8 w-[500px] overflow-clip rounded-xl text-white bg-gray-800/60 backdrop-blur-md text-base shadow-i-lg ${isActive && isPopupReady ? "hover-popup" : ""}`}
-                style={{top: position.top, left: position.left, right: position.right, bottom: position.bottom, display: isActive && isPopupReady ? "block" : "none"}}>
-                {images[0] && <img onLoad={() => setPopupReady(true)} src={images[0].url} className="max-h-[200px] w-full object-cover"/>}
-                {title && <h1 className="px-4 py-2 text-lg font-bold">{title}</h1>}
-                {description && (
-                    <div className="flex flex-col gap-2 px-4 pb-4">
-                        {description.split("\n").map((content) => (<p key={content.slice(0, 10)}>{content}</p>))}
-                    </div>
-                )}
-        </div>
+    // Render popup with basic data
+    setTitle(resp.meta.title)
+    setDescription(resp.meta.description + "\n")
+    setImages([resp.meta.image])
+    setActive(true)
+    // Use OpenAI API to generate a summary
+    const reader = new Readability(
+      new DOMParser().parseFromString(resp.html, "text/html")
     )
+    const parsed = reader.parse()
+    const messages = [
+      { role: "system", content: config.prompt },
+      { role: "user", content: parsed.textContent.slice(0, config.inputTokens * 3) },
+      { role: "assistant", content: resp.meta.description }
+    ] as ChatCompletionMessageParam[]
+    await streamOpenAICompletion(messages, setDescription, config).catch((e) =>
+      setDescription((prev) => prev + "\n" + "Error fetching data: " + e)
+    )
+    // Further updates to the popup are handled by the streaming function
+  }
+
+  const streamOpenAICompletion = async (
+    messages: ChatCompletionMessageParam[],
+    output: React.Dispatch<React.SetStateAction<string>>,
+    config: { apiKey: string; baseURL: string; model: string, outputTokens: number }
+  ) => {
+    const openai = new OpenAI({
+      apiKey: config.apiKey,
+      baseURL: config.baseURL,
+      dangerouslyAllowBrowser: true // it is a browser extension so this is okay!!
+    })
+
+    const stream = await openai.chat.completions.create({
+      model: config.model,
+      messages: messages,
+      stream: true,
+      max_tokens: config.outputTokens,
+    })
+    for await (const chunk of stream) {
+      if (!chunk.choices[0].delta) continue
+      output((prev) => prev + (chunk.choices[0].delta.content || ""))
+    }
+  }
+
+  useEffect(() => {
+    const callback = (event) => {
+      hoverTarget = (event.target as Element).closest("a")
+    }
+    document.addEventListener("mouseover", callback)
+    return () => {
+      document.removeEventListener("mouseover", callback)
+    }
+  })
+
+  useEffect(() => {
+    const callback = async (event) => {
+      if (event.key === "Control" && hoverTarget) {
+        if (!hoverTarget) {
+          return
+        }
+        setActive(false)
+        movePopup(hoverTarget)
+        await updatePopup()
+      }
+    }
+    window.addEventListener("keydown", callback)
+    return () => {
+      window.removeEventListener("keydown", callback)
+    }
+  })
+
+  useEffect(() => {
+    const callback = (event) => {
+      if (event.target !== hoverTarget) {
+        setActive(false)
+      }
+    }
+    window.addEventListener("scroll", callback)
+    return () => {
+      window.removeEventListener("scroll", callback)
+    }
+  })
+
+  return (
+    <div
+      className={`fixed min-h-8 w-[500px] overflow-clip rounded-xl text-white bg-gray-800/60 backdrop-blur-md text-base shadow-i-lg ${isActive && isPopupReady ? "hover-popup" : ""}`}
+      style={{
+        top: position.top,
+        left: position.left,
+        right: position.right,
+        bottom: position.bottom,
+        display: isActive && isPopupReady ? "block" : "none"
+      }}>
+      {images[0] && (
+        <img
+          onLoad={() => setPopupReady(true)}
+          src={images[0].url}
+          className="max-h-[200px] w-full object-cover"
+        />
+      )}
+      {title && <h1 className="px-4 py-2 text-lg font-bold">{title}</h1>}
+      {description && (
+        <div className="flex flex-col gap-2 px-4 pb-4">
+          {description.split("\n").map((content) => (
+            <p key={content.slice(0, 10)}>{content}</p>
+          ))}
+        </div>
+      )}
+    </div>
+  )
 }
 
-export default summaryPopup;
+export default summaryPopup
--- src/options/index.tsx
+++ src/options/index.tsx
@@ -1,32 +1,141 @@
 import { useStorage } from "@plasmohq/storage/hook"
-import "./options.css"
 
-export const defaults = {
+import "./styles.css"
+
+import { useState } from "react"
+
+const defaults = {
   baseURL: "https://api.openai.com/v1/",
   model: "gpt-3.5-turbo",
-  prompt: "Generate a descriptive short summary for the following web page content, avoiding clickbait, advertising or sensationalism; merely try to concisely summarize the page."
+  prompt:
+    "Generate a descriptive and concise summary for the following web page, avoiding emojis and lists.",
+  inputTokens: 300,
+  outputTokens: 100
 }
+
+const placeholderText = `In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available. It is also used to temporarily replace text in a process called greeking, which allows designers to consider the form of a webpage or publication, without the meaning of the text influencing the design.
+
+Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin. The first two words themselves are a truncation of dolorem ipsum ("pain itself").
+
+Versions of the Lorem ipsum text have been used in typesetting at least since the 1960s, when it was popularized by advertisements for Letraset transfer sheets. Lorem ipsum was introduced to the digital world in the mid-1980s, when Aldus employed it in graphic and word-processing templates for its desktop publishing program PageMaker. Other popular word processors, including Pages and Microsoft Word, have since adopted Lorem ipsum, as have many LaTeX packages, web content managers such as Joomla! and WordPress, and CSS libraries such as Semantic UI.`
 
 function IndexOptions() {
   const [apiKey, setApiKey] = useStorage("openai-key")
   const [baseURL, setBaseURL] = useStorage("openai-baseURL", defaults.baseURL)
   const [model, setModel] = useStorage("openai-model", defaults.model)
   const [prompt, setPrompt] = useStorage("system-prompt", defaults.prompt)
+  const [inputTokens, setInputTokens] = useStorage(
+    "input-tokens",
+    defaults.inputTokens
+  )
+  const [outputTokens, setOutputTokens] = useStorage(
+    "output-tokens",
+    defaults.outputTokens
+  )
+  const [inputTokenCost, setInputTokenCost] = useState(0.5) // Per million tokens
+  const [outputTokenCost, setOutputTokenCost] = useState(1.5)
 
   return (
-    <div className="w-[512px] bg-neutral-800 p-4 text-white"> 
-      <h1 className="mb-4 text-xl font-semibold">Options</h1>
-      <div id="optionsGrid">
+    <main className="bg-neutral-800 text-white">
+      <fieldset className="options-grid">
+        <legend>Options</legend>
         <label htmlFor="key">API Key</label>
-        <input type="password" id="key" value={apiKey} onChange={(e) => setApiKey(e.target.value)} />
+        <input
+          type="password"
+          id="key"
+          value={apiKey}
+          onChange={(e) => setApiKey(e.target.value)}
+        />
         <label htmlFor="baseURL">Base URL</label>
-        <input id="baseURL" value={baseURL} onChange={(e) => setBaseURL(e.target.value)} />
+        <input
+          id="baseURL"
+          value={baseURL}
+          onChange={(e) => setBaseURL(e.target.value)}
+        />
         <label htmlFor="model">Model Name</label>
-        <input id="model" value={model} onChange={(e) => setModel(e.target.value)} />
+        <input
+          id="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+        />
         <label htmlFor="prompt">Prompt</label>
-        <textarea id="prompt" value={prompt} onChange={(e) => setPrompt(e.target.value)} />
-      </div>
-    </div>
+        <textarea
+          id="prompt"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+        />
+      </fieldset>
+      <fieldset>
+        <legend>Tokens</legend>
+        <div className="options-grid">
+          <label htmlFor="input-tokens">Input Tokens</label>
+          <input
+            id="input-tokens"
+            value={inputTokens || 0}
+            onChange={(e) => setInputTokens(parseInt(e.target.value))}
+          />
+          <label htmlFor="output-tokens">Output Tokens</label>
+          <input
+            id="output-tokens"
+            value={outputTokens || 0}
+            onChange={(e) => setOutputTokens(parseInt(e.target.value))}
+          />
+        </div>
+        <h2 className="text-lg font-semibold">Examples:</h2>
+        <p className="italic">
+          These examples help you get an idea of what the model will "see". The
+          language model may choose to stop summarizing earlier than the maximum
+          output tokens.
+        </p>
+        <p>Input: {placeholderText.slice(0, inputTokens * 3) + "..."}</p>
+        <p>Output: {placeholderText.slice(0, outputTokens * 3) + "..."}</p>
+        <p className="italic">
+          This extension uses an estimate of ~3 characters per token.
+        </p>
+      </fieldset>
+      <fieldset>
+        <legend>Cost Calculator</legend>
+        <a href="https://openai.com/api/pricing/" className="underline">
+          OpenAI pricing
+        </a>
+        <div className="options-grid">
+          <label htmlFor="input-token-cost">Input Token Cost</label>
+          <span>
+            $
+            <input
+              id="input-token-cost"
+              value={inputTokenCost}
+              onChange={(e) => setInputTokenCost(parseFloat(e.target.value))}
+            />
+            /1M tokens
+          </span>
+          <label htmlFor="output-token-cost">Output Token Cost</label>
+          <span>
+            $
+            <input
+              id="output-token-cost"
+              value={outputTokenCost}
+              onChange={(e) => setOutputTokenCost(parseFloat(e.target.value))}
+            />
+            /1M tokens
+          </span>
+        </div>
+        <div className="options-grid">
+          <p>System Prompt</p>
+          <p>{`${Math.ceil(prompt.length / 3)} tokens * $${inputTokenCost.toFixed(2)} / 1M`}</p>
+          <p>Input Tokens</p>
+          <p>{`${inputTokens} tokens * $${inputTokenCost.toFixed(2)} / 1M`}</p>
+          <p>Output Tokens</p>
+          <p>{`${outputTokens} tokens * $${outputTokenCost.toFixed(2)} / 1M`}</p>
+        </div>
+        <p>{`= ~$${(((Math.ceil(prompt.length / 3) + inputTokens) * inputTokenCost) / 1e6 + (outputTokens * outputTokenCost) / 1e6).toPrecision(3)} per summarization`}</p>
+        <p className="italic">
+          This number is a lower estimate. The extension sends the article title
+          and prewritten summary to the summarization endpoint, in addition to
+          the article text and system prompt.
+        </p>
+      </fieldset>
+    </main>
   )
 }
 
