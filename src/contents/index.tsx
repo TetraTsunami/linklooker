0c9fa82b5d906e19c3e66fed9d73e9033c3365ce
Tsuni <akamitsunami@gmail.com>
2024-05-26 12:26:28 -0500

    Refactor content, WIP

--- src/contents/index.tsx
+++ src/contents/index.tsx
@@ -11,8 +11,15 @@ import type { PlasmoCSConfig } from "plasmo"
 export const config: PlasmoCSConfig = {
     matches: ["<all_urls>"],
     exclude_matches: ["*://*.wikipedia.com/*", "*://*.google.com/*",  "*://x.com/*"],
+  }
+  
+export const getStyle = () => {
+  const style = document.createElement("style")
+  style.textContent = cssText
+  return style
 }
-const defaults = {
+
+const defaultSettings = {
   baseURL: "https://api.openai.com/v1/",
   model: "gpt-3.5-turbo",
   prompt:
@@ -21,11 +28,16 @@ const defaults = {
   outputTokens: 100
 }
 
-export const getStyle = () => {
-  const style = document.createElement("style")
-  style.textContent = cssText
-  return style
+interface Meta {
+  title: string,
+  description?: string,
+  image?: {
+    url: string,
+    width?: string,
+    height?: string
+  },
 }
+
 
 const settings = new Storage()
 
@@ -35,14 +47,15 @@ const getConfig = async () => {
   // Grab our config
   const config = {
     apiKey: await settings.get("openai-key"),
-    baseURL: (await settings.get("openai-baseURL")) || defaults.baseURL,
-    model: (await settings.get("openai-model")) || defaults.model,
-    prompt: (await settings.get("system-prompt")) || defaults.prompt,
-    inputTokens: (parseInt(await settings.get("input-tokens"))) || defaults.inputTokens,
-    outputTokens: (parseInt(await settings.get("output-tokens"))) || defaults.outputTokens,
+    baseURL: (await settings.get("openai-baseURL")) || defaultSettings.baseURL,
+    model: (await settings.get("openai-model")) || defaultSettings.model,
+    prompt: (await settings.get("system-prompt")) || defaultSettings.prompt,
+    inputTokens: (parseInt(await settings.get("input-tokens"))) || defaultSettings.inputTokens,
+    outputTokens: (parseInt(await settings.get("output-tokens"))) || defaultSettings.outputTokens,
   }
   return config
 }
+
 const summaryPopup = () => {
   const [position, setPosition] = useState({ top: 0, left: 0 } as {
     top?: number
@@ -53,9 +66,22 @@ const summaryPopup = () => {
   const [isActive, setActive] = useState(false) // Is the user trying to open the popup
   const [isPopupReady, setPopupReady] = useState(false) // Is the popup ready to be displayed
   const [title, setTitle] = useState("")
-  const [images, setImages] = useState([])
+  const [image, setImage] = useState({
+    url: "",
+    width: "",
+    height: "",
+  })
   const [description, setDescription] = useState("")
 
+  const resetState = () => {
+    setPopupReady(false)
+    setDescription("")
+  }
+
+  /**
+   * Places the popup relative to the target element such that it is visible.
+   * @param target The target element to place the popup relative to
+   */
   const movePopup = (target: Element) => {
     // Decide where to place
     const bounds = target.getBoundingClientRect()
@@ -70,66 +96,70 @@ const summaryPopup = () => {
     setPosition({ ...vertical, ...horizontal })
   }
 
-  const updatePopup = async () => {
-    // Validate URL
-    const text = hoverTarget.textContent
+  /**
+   * Get the URL destination of the target link
+   * @param target A link
+   * @returns A URL
+   * @throws If the URL cannot be found, or this element cannot handle the URL
+   */
+  const getURL = (target: Element) => {
     let url = ""
     try {
       url = hoverTarget.getAttribute("href")
     } catch (_) {
-      return
+      throw new Error("Could not get URL")
     }
     if (!url || url.startsWith("#") || url.startsWith("javascript")) {
-      return
+      throw new Error("Invalid URL")
     }
     if (url.startsWith("/")) {
       url = window.location.origin + url
     }
-    // Reset state & fetch configuration
-    setPopupReady(false)
-    setDescription("")
-    const config = await getConfig()
-    // Fetch basic data & summary
-    const resp = await sendToBackground({ name: "scrape", body: { url } })
-    if (resp.error) {
-      setTitle("Error fetching data")
-      setDescription(`${resp.error}`)
-      return
-    }
-    const document = new DOMParser().parseFromString(resp.html, "text/html")
-    if (!isProbablyReaderable(document)) return // We can't really do anything with this
-    const reader = new Readability(document)
-    const parsed = reader.parse()
-    // Render popup with basic data
-    setTitle(resp.meta.title || parsed.title)
-    setDescription("")
-    setImages([resp.meta.image])
-    setActive(true)
-    if (!resp.meta.image) {
-      setPopupReady(true)
-    } else {
-      setTimeout(() => setPopupReady(true), 1000)
-    }
-    // Use OpenAI API to generate a summary
-    const messages = [
-      { role: "system", content: config.prompt },
-      { role: "user", content: `Context: "${text}"\nContent: "${parsed.textContent.slice(0, config.inputTokens * 3)}"` },
-    ] as ChatCompletionMessageParam[]
-    await streamOpenAICompletion(messages, setDescription, config).catch((e) =>
-      setDescription((prev) => prev + "\n" + "Error fetching data: " + e)
-    )
-    // Further updates to the popup are handled by the streaming function
+    return url
   }
 
-  const streamOpenAICompletion = async (
-    messages: ChatCompletionMessageParam[],
-    output: React.Dispatch<React.SetStateAction<string>>,
-    config: { apiKey: string; baseURL: string; model: string, outputTokens: number }
-  ) => {
+  /**
+   * Fetches the "structural" data of a webpage.
+   * This includes its title and description (OpenGraph/Head tags), and parsed body text content (Readability.js)
+   * @param url The URL of the webpage
+   * @returns An object containing the meta data and parsed content {meta, readability}
+   */
+  const getTagData = async (url: string) => {
+    const resp = await sendToBackground({ name: "scrape", body: { url } })
+    if (resp.error) {
+      throw new Error(resp.error)
+    }
+    const document = new DOMParser().parseFromString(resp.html, "text/html")
+    const reader = new Readability(document)
+    const parsed = reader.parse()
+    const tags = resp.meta as Meta // Data from html tags
+    // There's some overlap, so we'll return a merged object with only the keys we need
+    return {
+      title: tags.title || parsed.title,
+      description: tags.description || parsed.excerpt,
+      image: tags.image,
+      body: parsed.textContent,
+      siteName: parsed.siteName,
+    }
+  }
+
+  /**
+   * Uses the OpenAI API to generate a more extensive summary for the given content. Output is appended to the description state.
+   * @param tagData The data to generate a summary for
+   */
+  const getOAIData = async (tagData: { title: string; description: string; image: { url: string; width?: string; height?: string }; body: string; siteName: string }) => {
+    const config = await getConfig()
+    // Maybe the text of the link is ambiguous and the user wants to know how the content relates
+    const linkText = hoverTarget.textContent
+    const messages = [
+      { role: "system", content: config.prompt },
+      { role: "user", content: `Context: "${linkText}"\nContent: "${tagData.body.slice(0, config.inputTokens * 3)}[...]"` },
+      { role: "assistant", content: `#${tagData.title}\n${tagData.description}`}
+    ] as ChatCompletionMessageParam[]
     const openai = new OpenAI({
       apiKey: config.apiKey,
       baseURL: config.baseURL,
-      dangerouslyAllowBrowser: true // it is a browser extension so this is okay!!
+      dangerouslyAllowBrowser: true // It is a browser extension, so this is okay
     })
 
     const stream = await openai.chat.completions.create({
@@ -138,14 +168,42 @@ const summaryPopup = () => {
       stream: true,
       max_tokens: config.outputTokens,
     })
+    setDescription((prev) => prev + "\n")
     for await (const chunk of stream) {
       if (!chunk.choices[0].delta) continue
-      output((prev) => prev + (chunk.choices[0].delta.content || ""))
+      setDescription((prev) => prev + (chunk.choices[0].delta.content || ""))
+    }
+  }
+
+  const renderTagPopup = (tagData: { title: any; description: string; image: any }) => {
+    setTitle(tagData.title)
+    setImage(tagData.image)
+    setDescription(tagData.description)
+    setActive(true)
+    if (!tagData.image) {
+      setPopupReady(true)
+    } else {
+      setTimeout(() => setPopupReady(true), 1000) // Wait for image to load
+    }
+  }
+
+  const updatePopup = async () => {
+    try {
+      const url = getURL(hoverTarget)
+      resetState()
+      const tagData = await getTagData(url)
+      renderTagPopup(tagData)
+      if (tagData.description.length < 200) { // If the description is too short, we'll try to get more data
+        await getOAIData(tagData)
+      }
+    } catch (e) {
+      console.error(e)
+      resetState()
     }
   }
 
   useEffect(() => {
-    const callback = (event) => {
+    const callback = (event: { target: Element }) => {
       hoverTarget = (event.target as Element).closest("a")
     }
     document.addEventListener("mouseover", callback)
@@ -155,7 +213,7 @@ const summaryPopup = () => {
   })
 
   useEffect(() => {
-    const callback = async (event) => {
+    const callback = async (event: { key: string }) => {
       if (event.key === "Control" && hoverTarget) {
         if (!hoverTarget) {
           return
@@ -172,7 +230,7 @@ const summaryPopup = () => {
   })
 
   useEffect(() => {
-    const callback = (event) => {
+    const callback = (event: { target: Element }) => {
       if (event.target !== hoverTarget) {
         setActive(false)
       }
@@ -188,6 +246,8 @@ const summaryPopup = () => {
     hoverTarget.getAttribute("href")
   } catch (_) {}
 
+  // If it's got transparency, we don't want to cut it off (could be icon or logo) = use contain. Otherwise, it looks prettier to use cover
+  const imageType = image && image.url && image.url.includes("png") ? "image-contain" : "image-cover"
   return (
     <div
       className={`fixed min-h-8 w-[500px] overflow-clip rounded-xl text-white bg-gray-800/60 backdrop-blur-md text-base shadow-i-lg ${isActive && isPopupReady ? "hover-popup" : ""}`}
@@ -198,11 +258,11 @@ const summaryPopup = () => {
         bottom: position.bottom,
         display: isActive && isPopupReady ? "block" : "none"
       }}>
-      {images[0] && (
+      {image && (
         <img
           onLoad={() => setPopupReady(true)}
-          src={images[0].url}
-          className="max-h-[200px] w-full object-cover"
+          src={image.url}
+          className={imageType}
         />
       )}
 
