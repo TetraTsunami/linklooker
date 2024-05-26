1d6859007065635202dcf4051a84b1ce95326f84
Tsuni <akamitsunami@gmail.com>
2024-05-26 12:26:04 -0500

    Simplify scraper

--- src/background/messages/scrape.ts
+++ src/background/messages/scrape.ts
@@ -5,7 +5,7 @@ const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
   try {
     const resp = await fetch(req.body.url)
     const html = await resp.text()
-    const meta = parse(html)
+    const meta = parse(html, req.body.url)
     res.send({
       meta,
       html,
@@ -24,111 +24,18 @@ interface Meta {
     height?: string
   },
 }
-const shorthandProperties = {
-  "image": "image:url",
-  "video": "video:url",
-  "audio": "audio:url"
-}
 
-const keyBlacklist = [
-  '__proto__',
-  'constructor',
-  'prototype'
-]
-
-const parse = (html: string): Meta => {
-  // Subset of open-graph parser (https://github.com/samholmes/node-open-graph/blob/master/index.js)
+const parse = (html: string, url: string) => {
   const $ = cheerio.load(html);
-  // Check for xml namespace
-
-  let namespace,
-    $html = $('html');
-
-  if ($html.length) {
-    var attribKeys = Object.keys($html[0].attribs);
-
-    attribKeys.some(function (attrName) {
-      var attrValue = $html.attr(attrName);
-
-      if (attrValue.toLowerCase() === 'http://opengraphprotocol.org/schema/'
-        && attrName.substring(0, 6) == 'xmlns:') {
-        namespace = attrName.substring(6);
-        return false;
-      }
-    })
+  const title = $('meta[property="og:title"]').attr('content') || $('title').text();
+  const description = $('meta[property="og:description"]').attr('content') || $('meta[name="description"]').attr('content');
+  const ogImage ={
+    url: $('meta[property="og:image"]').attr('content') || $('meta[property="og:image:url"]').attr('content'),
+    width: $('meta[property="og:image:width"]').attr('content'),
+    height: $('meta[property="og:image:height"]').attr('content')
   }
-  namespace = namespace || "og";
-
-  var meta = {},
-    metaTags = $('meta');
-
-  metaTags.each(function () {
-    var element = $(this),
-      propertyAttr = element.attr('property');
-
-    // If meta element isn't an "og:" property, skip it
-    if (!propertyAttr || propertyAttr.substring(0, namespace.length) !== namespace)
-      return;
-
-    var property = propertyAttr.substring(namespace.length + 1),
-      content = element.attr('content');
-
-    // If property is a shorthand for a longer property,
-    // Use the full property
-    property = shorthandProperties[property] || property;
-
-
-    var key, tmp,
-      ptr = meta,
-      keys = property.split(':', 4);
-
-    // we want to leave one key to assign to so we always use references
-    // as long as there's one key left, we're dealing with a sub-node and not a value
-
-    while (keys.length > 1) {
-      key = keys.shift();
-
-      if (keyBlacklist.includes(key)) continue
-
-      if (Array.isArray(ptr[key])) {
-        // the last index of ptr[key] should become
-        // the object we are examining.
-        tmp = ptr[key].length - 1;
-        ptr = ptr[key];
-        key = tmp;
-      }
-
-      if (typeof ptr[key] === 'string') {
-        // if it's a string, convert it
-        ptr[key] = { '': ptr[key] };
-      } else if (ptr[key] === undefined) {
-        // create a new key
-        ptr[key] = {};
-      }
-
-      // move our pointer to the next subnode
-      ptr = ptr[key];
-    }
-
-    // deal with the last key
-    key = keys.shift();
-
-    if (ptr[key] === undefined) {
-      ptr[key] = content;
-    } else if (Array.isArray(ptr[key])) {
-      ptr[key].push(content);
-    } else {
-      ptr[key] = [ptr[key], content];
-    }
-  });
-
-  // Fallbacks for title and image
-  if (!meta.hasOwnProperty('title')) {
-    meta['title'] = $('title').text();
-  }
-
-  // TODO: could be an array of images instead
-  if (!meta.hasOwnProperty('image')) {
+  const image = ogImage.url ? ogImage : (() => {
+    // If we don't have an image handed to us, take the first image in the body of the page
     const img = $('img');
 
     if (img.length) {
@@ -137,21 +44,25 @@ const parse = (html: string): Meta => {
         width?: string,
         height?: string
       };
-      imgObj.url = $('img').attr('src');
+      const src = img.attr('src');
+      // The src might be relative, so we need to convert it to an absolute URL
+      if (src && src.startsWith('http')) {
+        imgObj.url = src;
+      } else {
+        imgObj.url = new URL(src, url).href;
+      }
 
       // Set image width and height properties if respective attributes exist
-      if ($('img').attr('width'))
-        imgObj.width = $('img').attr('width');
-      if ($('img').attr('height'))
-        imgObj.height = $('img').attr('height');
-
-      meta['image'] = imgObj;
+      if (img.attr('width')) imgObj.width = img.attr('width');
+      if (img.attr('height')) imgObj.height = img.attr('height');
+      return imgObj;
     }
-
-  }
-
-  return meta as Meta;
-
+  })();
+  return {
+    title,
+    description,
+    image,
+  } as Meta
 }
 
 
