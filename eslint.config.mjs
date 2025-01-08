6a8a28623544866beaf7e51b8dde4da2da061cfb
Tsuni <78718829+TetraTsunami@users.noreply.github.com>
2025-01-08 12:29:58 -0600

    Add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import pluginJs from "@eslint/js"
+import pluginReact from "eslint-plugin-react"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off"
+    }
+  }
+]
--- src/background/common.ts
+++ src/background/common.ts
@@ -1,5 +1,5 @@
 export const installedHandler = (object) => {
-  let internalUrl = chrome.runtime.getURL("tabs/getting-started.html");
+  const internalUrl = chrome.runtime.getURL("tabs/getting-started.html");
 
   if (object.reason === chrome.runtime.OnInstalledReason.INSTALL) {
     chrome.tabs.create({ url: internalUrl });
